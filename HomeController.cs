750925c77d348ad051fb02cf705bc584b741392e
PrashantSNimbalkar <107276876+PrashantSNimbalkar@users.noreply.github.com>
2022-07-26 14:20:27 +0530

    Add Files Via upload

---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DemoAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize(Roles ="Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles ="Customer")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
